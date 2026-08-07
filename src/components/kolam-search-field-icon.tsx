773c8378aba3dd707deb0f75822d60da0ce348f7
BJM15 <antilamers8@gmail.com>
2026-08-07 17:33:19 +0700

    Use custom lup icon for search fields

--- src/components/kolam-search-field-icon.tsx
+++ src/components/kolam-search-field-icon.tsx
@@ -1,12 +1,19 @@
-﻿import React from 'react';
+import React from 'react';
 import {View} from 'react-native';
+import {SvgXml} from 'react-native-svg';
+import {KOLAM_LUP_SEARCH_ICON_SVG} from '../assets/icons/lup-search-icon-svg';
+import {kolamVisualTokens as V} from '../domain/kolam-visual';
 import {searchFieldStyles as styles} from './kolam-search-field-styles';
+
+const LUP_SEARCH_ICON_XML = KOLAM_LUP_SEARCH_ICON_SVG.replace(
+  /currentColor/g,
+  V.colors.mutedFg,
+);
 
 export function KolamSearchFieldIcon() {
   return (
     <View style={styles.icon}>
-      <View style={styles.lens} />
-      <View style={styles.handle} />
+      <SvgXml height="100%" width="100%" xml={LUP_SEARCH_ICON_XML} />
     </View>
   );
 }
